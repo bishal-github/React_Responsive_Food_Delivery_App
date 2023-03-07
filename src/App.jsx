97754d3367f5cc0d9b9e0ecfb73ccbd33371e85a
bishal-github <shreesbishal44@gmail.com>
2023-03-07 19:50:38 +0545

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HeadlineCard from './components/HeadlineCard';
+import Food from './components/Food';
+import Category from './components/Category';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <HeadlineCard />
+      <Food />
+      <Category />
     </div>
   )
 }
